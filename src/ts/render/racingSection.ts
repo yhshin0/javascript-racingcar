427608de5049db72285db5e3761405721a42912c
yhshin0 <51621520+yhshin0@users.noreply.github.com>
2021-06-07 20:03:16 +0900

    delete: racingSection.ts

--- src/ts/render/racingSection.ts
+++
@@ -1,9 +0,0 @@
-export default class RacingSection {
-  render(): string {
-    const racingCarNames = [];
-    return `<section class="d-flex justify-center mt-5">
-    <div class="mt-4 d-flex">
-    </div>
-  </section>`;
-  }
-}
